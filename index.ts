7a4cdfc946366311d935f90053cdbcea7e8c2938
karatalexx <karatalexx@gmail.com>
2019-08-21 20:55:50 +0300

    added tests

--- index.ts
+++ index.ts
@@ -41,3 +41,5 @@ app.post('/add', urlencodedParser, function(req, res) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000');
 });
+
+module.exports = app;
---
+++ tests/test.js
@@ -0,0 +1,42 @@
+const mocha = require('ts-mocha');
+const chai = require('chai');
+// import * as chai from "chai";
+const chaiHttp = require('chai-http');
+const server = require('../index.ts');
+// let should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Add a product test', () => {
+  it('Checks if the product exists', () => {
+    const request = {
+      'product_name': 'prod1',
+      'qty': 'good',
+      'unit_price': '111.11',
+      'user_email': 'test@prod.com'
+    };
+    chai.request(server)
+      .post('/add')
+      .type('form')
+      .send(request)
+      .end((err, res) => {
+        expect(res.text).to.equal('Product exists');
+      });
+  });
+  it('Checks if the product added', () => {
+    const request = {
+      'product_name': 'prod11',
+      'qty': 'good',
+      'unit_price': '111.11',
+      'user_email': 'test@prod.com'
+    };
+    chai.request(server)
+      .post('/add')
+      .type('form')
+      .send(request)
+      .end((err, res) => {
+        expect(res.text).to.equal('Product added to the database');
+      });
+  });
+});
