f0974bd204423e1ce8c0bba4d140371527a2cc5e
alexpan <p.aleksey@do-it.co>
2019-08-22 14:21:14 +0300

    Add a post route to add new cart item in table cart

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
-import express = require('express');
+import express from 'express';
+import {CartoController, createValidator} from "./controllers/carto";
+import {isValid} from "./utils";
+
+const controller = new CartoController();
 
 // Create a new express application instance
 const app: express.Application = express();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.use(urlencodedParser);
+
+app.post('/add', isValid(createValidator), controller.add);
+
+app.listen(process.env.PORT, function() {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
-});
+module.exports = app;
