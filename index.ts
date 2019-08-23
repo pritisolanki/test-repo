0e288ad47036b8be7986bc872e9536312b32e43e
shubhnagi singh <shubhnagi.singh@smartdatainc.net>
2019-08-23 12:07:43 +0530

    First commit

--- index.ts
+++ index.ts
@@ -3,10 +3,11 @@ import express = require('express');
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  console.log("testing")
 });
